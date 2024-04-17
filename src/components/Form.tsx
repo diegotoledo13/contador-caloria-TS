b31b31d7dd924def603c9b83d2db818227d3f3c6
diegotoledo13 <diegomasteruno13@gmail.com>
2024-04-17 14:07:22 -0300

    comenzando proyecto en react y typeScript

---
+++ src/components/Form.tsx
@@ -0,0 +1,49 @@
+import { categories } from "../data/categories";
+export default function Form() {
+  return (
+    <form className=" space-y-5 bg-white shadow p-10 rounded-lg">
+      <div className=" grid grid-cols-1 gap-3">
+        <label htmlFor="category" className=" font-bold">
+          Categoría:
+        </label>
+        <select
+          className=" border border-slate-300 p-2 rounded-lg w-full bg-white"
+          id="category"
+        >
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className=" grid grid-cols-1 gap-3">
+        <label htmlFor="activity" className=" font-bold">
+          Actividad:
+        </label>
+        <input
+          id="activity"
+          type="text"
+          className=" border border-slate-300 p-2 rounded-lg"
+          placeholder="Ejemplo: Comida, Jugo de naranja, Ensalada, Ejercicio, Pesas, Bicicleta, etc.."
+        />
+      </div>
+      <div className=" grid grid-cols-1 gap-3">
+        <label htmlFor="calories" className=" font-bold">
+          Calorias:
+        </label>
+        <input
+          id="calories"
+          type="number"
+          className=" border border-slate-300 p-2 rounded-lg"
+          placeholder="Calorias: 300, 500 etc.."
+        />
+      </div>
+      <input
+        type="submit"
+        value="Guardar comida o ejercicio"
+        className=" bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer rounded-lg"
+      />
+    </form>
+  );
+}
