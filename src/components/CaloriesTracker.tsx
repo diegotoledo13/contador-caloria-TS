c354ccdd28ae323c30a8dfb71f280b02805efa46
diegotoledo13 <diegomasteruno13@gmail.com>
2024-05-12 10:52:57 -0300

    agregando resumen de calorias usando useMemo

---
+++ src/components/CaloriesTracker.tsx
@@ -0,0 +1,33 @@
+import { useMemo } from "react";
+import type { Activity } from "../types/index";
+
+type CaloriesTrackerProps = {
+  activities: Activity[];
+};
+
+export default function CaloriesTracker({ activities }: CaloriesTrackerProps) {
+  const caloriesConsume = useMemo(
+    () =>
+      activities.reduce(
+        (total, activity) =>
+          activity.category === 1 ? total + activity.calories : total,
+        0
+      ),
+    [activities]
+  );
+  return (
+    <>
+      <h2 className=" text-4xl font-black text-white text-center">
+        Resumen de Calorias
+      </h2>
+      <div className=" flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+        <p className=" text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+          <span className=" font-black text-6xl text-orange">
+            {caloriesConsume}
+          </span>
+          <span>Calorias Consumidas</span>
+        </p>
+      </div>
+    </>
+  );
+}
